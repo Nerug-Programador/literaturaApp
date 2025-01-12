0ef8adad5f30cdb86703f60e3aa3721415ebbd3f
Nerug-Programador <williamgermanarangurenbuirago@gmail.com>
2025-01-12 13:47:26 -0500

    adicionando Proyecto Spring

---
+++ src/main/java/com/nerugdev/literaturaApp/LiteraturaAppApplication.java
@@ -0,0 +1,13 @@
+package com.nerugdev.literaturaApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraturaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraturaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nerugdev/literaturaApp/LiteraturaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerugdev.literaturaApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraturaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
